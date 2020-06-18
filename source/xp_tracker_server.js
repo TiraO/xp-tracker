4f9675b4ac5726477cade7f60e46d4b92bdb61e9
Tira Odhner <atiraodhner@gmail.com>
2020-06-17 20:49:11 -0400

    make an end point for saving assignments

--- source/xp_tracker_server.js
+++ source/xp_tracker_server.js
@@ -1,29 +1,39 @@
 let express = require('express');
 const { Pool, Client } = require('pg')
 let app = express();
-
+let bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3002;
+
+app.use(bodyParser.json({ type: 'application/*+json' }));
+let jsonParser = bodyParser.json();
+
+app.post("/assignments", jsonParser, (request, response) => {
+  console.log(request.body);
+  addScore(request.body.person, request.body.score, request.body.description);
+  response.send("some text");
+});
+
 app.listen(PORT, function () {
-    console.log('App listening on port ' + PORT);
+  console.log('App listening on port ' + PORT);
 });
 
 const pool = new Pool({
-    user: 'app_admin',
-    host: 'localhost',
-    database: 'xptracker',
-    password: 'password123',
-    port: 5432,
+  user: 'app_admin',
+  host: 'localhost',
+  database: 'xptracker',
+  password: 'password123',
+  port: 5432,
 });
 
 let addScore = async (person, score, description) => {
-    let assignment = {
-        person: person,
-        score: score,
-        description: description,
-    };
-    await pool.query('BEGIN');
-    await pool.query("INSERT INTO assignments (person, score, description) VALUES ($1, $2, $3);", [person, score, description])
-    await pool.query('COMMIT');
+  let assignment = {
+    person: person,
+    score: score,
+    description: description,
+  };
+  await pool.query('BEGIN');
+  await pool.query("INSERT INTO assignments (person, score, description) VALUES ($1, $2, $3);", [person, score, description])
+  await pool.query('COMMIT');
 
 };
 addScore("Hamad", 100, "Mission Log 2");
